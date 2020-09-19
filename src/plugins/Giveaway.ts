937f3e4c53ab6362564a102fd97b57f080f8bb97
Sayakie <sayakie.ovo@gmail.com>
2020-09-19 01:56:08 +0000

    stash: Hide plugins#Giveaway

--- src/plugins/Giveaway.ts
+++ src/plugins/Giveaway.ts
@@ -1,14 +1,9 @@
+/*
 import { EVENT } from '../helpers/Constants'
 import type { ModuleOptions } from '../types'
 
 export default ( { client }: ModuleOptions ): void => {
   client.incrementMaxListener()
   client.on(EVENT.MESSAGE_CREATE, message => {})
- }
-
- /**
-  * /글로벌
-  * /야생
-  * /월드
-  * /투토리얼
-  */
+}
+*/
