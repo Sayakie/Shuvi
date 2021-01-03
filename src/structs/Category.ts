de4283b3ecf7e5f5ee8eb5bfd874cb0c1a82e699
Sayakie <sayakie.ovo@gmail.com>
2021-01-03 18:00:14 +0000

    Add "Category.Moderation" category

--- src/structs/Category.ts
+++ src/structs/Category.ts
@@ -1,6 +1,7 @@
 export const enum Category {
   Uncategorized = 'Uncategorized',
   Administrative = 'Administrative',
+  Moderation = 'Moderation',
   Owner = 'Owner',
   Settings = 'Settings',
   Music = 'Music'
