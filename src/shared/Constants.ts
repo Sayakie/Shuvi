653dfc27ba82b05cce9238c6d6437565ba189e1e
Sayakie <sayakie.ovo@gmail.com>
2021-01-24 18:34:37 +0000

    Add all kinds of discord pattern

--- src/shared/Constants.ts
+++ src/shared/Constants.ts
@@ -38,3 +38,18 @@ export const SYMBOL = {
   WHITESPACE: ' ',
   NEWLINE: '\n'
 } as const
+
+export const PATTERN = {
+  MENTION: /<(?:(?:@[!&]?)|#)(?<id>\d+)>/i,
+  MENTION_USER: /<@!?(?<id>\d+)>/i,
+  MENTION_USER_ID: /<@(?<id>\d+)>/i,
+  MENTION_USER_NICKNAME: /<@!(?<id>\d+)>/i,
+  MENTION_CHANNEL: /<#(?<id>\d+)>/i,
+  MENTION_ROLE: /<@&(?<id>\d+)>/i,
+  EMOJI: /<(?<animation>a)?:(?<name>.+):(?<id>.+)>/i,
+  CUSTOM_EMOJI: /<:(?<name>.+):(?<id>.+)>/i,
+  ANIMATED_EMOJI: /<a:(?<name>.+):(?<id>.+)>/i,
+  ENGLISH_ONLY: /^[A-Za-z][A-Za-z0-9]*$/,
+  WHITESPACE: /\s+/g,
+  NEWLINES: /\n|\r|\r\n/
+}
